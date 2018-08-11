bed6d2491760bca7929d564d84f2b25da1f9cbb5
Ivan <madritistaaa@gmail.com>
2018-08-11 15:32:47 +0300

    Cleanup

--- utils/index.js
+++
@@ -1,9 +0,0 @@
-const messageSenderInit = require('./message-sender');
-
-module.exports = (config) => {
-  const messageSender = messageSenderInit(config.slack.token);
-
-  return {
-    messageSender
-  };
-}

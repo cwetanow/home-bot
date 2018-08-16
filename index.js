66f4651f1a8824959cadab3286a26a27bab21a60
Ivan <madritistaaa@gmail.com>
2018-08-16 10:33:46 +0300

    Change module selection

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ require('dotenv').config();
 
 const MODULES_DIR = './modules/';
 
-const modules = fs
+const modules = {};
+
+fs
   .readdirSync(MODULES_DIR)
-  .map(item => require(MODULES_DIR + item));
+  .forEach(item => {
+    modules[item.replace('.js', '')] = require(MODULES_DIR + item);
+  });
 
 const messageSender = require('./message-sender');
 
@@ -14,17 +18,9 @@ const onMessageSent = (message) => {
   // console.log(`Command: ${message}, Time: ${new Date()}`)
 
   const command = message.split(' ')[0];
-  let pendingResult = null;
 
-  modules.forEach(module => {
-    const moduleResult = module(command, message);
-    if (moduleResult) {
-      pendingResult = moduleResult;
-    }
-  });
-
-  if (pendingResult) {
-    pendingResult
+  if (modules[command]) {
+    modules[command](command, message)
       .then(response => {
         messageSender(response);
       });
@@ -33,16 +29,16 @@ const onMessageSent = (message) => {
 
 const { RTMClient } = require('@slack/client');
 
-const rtm = new RTMClient(process.env.SLACK_BOT_OAUTH_ACCESS_TOKEN);
-rtm.start();
-var channel = "#general";
-rtm.on('message', (event) => {
-  const message = event;
+// const rtm = new RTMClient(process.env.SLACK_BOT_OAUTH_ACCESS_TOKEN);
+// rtm.start();
+// var channel = "#general";
+// rtm.on('message', (event) => {
+//   const message = event;
 
-  if ((message.subtype && message.subtype === 'bot_message') ||
-    (!message.subtype && message.user === rtm.activeUserId)) {
-    return;
-  }
+//   if ((message.subtype && message.subtype === 'bot_message') ||
+//     (!message.subtype && message.user === rtm.activeUserId)) {
+//     return;
+//   }
 
-  onMessageSent(message.text);
-});
+//   onMessageSent(message.text);
+// });

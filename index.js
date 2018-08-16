f1984fb072cb4be87969fa57b418047fce2608c9
Ivan <madritistaaa@gmail.com>
2018-08-16 10:34:38 +0300

    Add message if not found command

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ fs
 const messageSender = require('./message-sender');
 
 const onMessageSent = (message) => {
-  // console.log(`Command: ${message}, Time: ${new Date()}`)
-
   const command = message.split(' ')[0];
 
   if (modules[command]) {
@@ -24,21 +22,23 @@ const onMessageSent = (message) => {
       .then(response => {
         messageSender(response);
       });
+  } else {
+    messageSender('I was not able to parse your command');
   }
 }
 
 const { RTMClient } = require('@slack/client');
 
-// const rtm = new RTMClient(process.env.SLACK_BOT_OAUTH_ACCESS_TOKEN);
-// rtm.start();
-// var channel = "#general";
-// rtm.on('message', (event) => {
-//   const message = event;
+const rtm = new RTMClient(process.env.SLACK_BOT_OAUTH_ACCESS_TOKEN);
+rtm.start();
+var channel = "#general";
+rtm.on('message', (event) => {
+  const message = event;
 
-//   if ((message.subtype && message.subtype === 'bot_message') ||
-//     (!message.subtype && message.user === rtm.activeUserId)) {
-//     return;
-//   }
+  if ((message.subtype && message.subtype === 'bot_message') ||
+    (!message.subtype && message.user === rtm.activeUserId)) {
+    return;
+  }
 
-//   onMessageSent(message.text);
-// });
+  onMessageSent(message.text);
+});

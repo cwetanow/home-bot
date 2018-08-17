9ccbfc85c620a5a6be7ad7752d1c07d0ceeada60
Ivan <madritistaaa@gmail.com>
2018-08-17 11:48:18 +0300

    Implement line info

--- modules/transport.js
+++ modules/transport.js
@@ -2,7 +2,7 @@ let request = require('request');
 
 const COMMAND_NAME = 'transport';
 
-let stops = [];
+let stops = null;
 let routes = [];
 
 let url = 'https://routes.sofiatraffic.bg/resources/stops-bg.json';
@@ -26,52 +26,109 @@ let url = 'https://routes.sofiatraffic.bg/resources/stops-bg.json';
   })
   .then(res => {
     routes = res;
-  })
+  });
 
+const transportTypes = {
+  bus: 'bus',
+  trolley: 'trolley',
+  tram: 'tram'
+}
+
+const getLine = (type, name) => {
+  const transportTypeLines = routes.find(r => r.type === type);
+
+  if (transportTypeLines) {
+    const line = transportTypeLines.lines.find(l => l.name.toLowerCase().indexOf(name.toLowerCase()) >= 0);
+
+    if (line) {
+      line.routes.forEach(route => {
+        route.stops = [];
+
+        route.codes.forEach(c => {
+          const stop = stops.find(s => s.code === c);
+          route.stops.push(stop)
+        })
+      });
+
+      return line;
+    }
+  }
+}
 
 module.exports = (command, message) => {
-  if (command.toLowerCase() === COMMAND_NAME) {
-    return new Promise((resolve, reject) => {
-      const params = message.split(' ');
+  return new Promise((resolve, reject) => {
+    const params = message.split(' ');
 
-      const type = params[1];
+    const type = params[1];
 
-      if (type === 'stop') {
-        const stopName = params[2].toLowerCase();
+    if (type === 'stop') {
+      const stopName = params[2].toLowerCase();
 
-        const matched = stops.filter(s => s.name.toLowerCase().indexOf(stopName) >= 0);
-        let result = '';
+      const matched = stops.filter(s => s.name.toLowerCase().indexOf(stopName) >= 0);
+      let result = '';
 
-        matched.forEach(stop => {
-          result += `${stop.name} ${stop.code}\n`;
+      matched.forEach(stop => {
+        result += `${stop.name} ${stop.code}\n`;
+      });
+
+      return resolve(result);
+    }
+
+    if (type === 'code') {
+      const code = params[2];
+
+      url = `https://api-arrivals.sofiatraffic.bg/api/v1/arrivals/${code}/`;
+
+      request.get(url, (err, response, body) => {
+        body = JSON.parse(body);
+
+        let result = `${body.name}\n`;
+
+        body.lines.forEach(line => {
+          result += `${line.vehicle_type} ${line.name} `;
+
+          line.arrivals.forEach(arrival => {
+            result += ` ${arrival.time}`;
+          })
+
+          result += '\n';
         });
 
         return resolve(result);
+      });
+    }
+
+    if (transportTypes.hasOwnProperty(type)) {
+      const name = params[2];
+
+      const line = getLine(type, name);
+
+      if (!line) {
+        return resolve('Line not found');
       }
 
-      if (type === 'code') {
-        const code = params[2];
+      let result = `${type} ${line.name}:\n`;
 
-        url = `https://api-arrivals.sofiatraffic.bg/api/v1/arrivals/${code}/`;
+      const isFull = params[3] && params[3] === 'full';
 
-        request.get(url, (err, response, body) => {
-          body = JSON.parse(body);
+      line.routes.forEach(route => {
+        if (isFull) {
+          route.stops.forEach(stop => {
+            result += `${stop.name} (${stop.code}) - `;
+          })
 
-          let result = `${body.name}\n`;
+          result += '\n';
+        } else {
+          const firstStop = route.stops[0];
+          const lastStop = route.stops[route.stops.length - 1];
 
-          body.lines.forEach(line => {
-            result += `${line.vehicle_type} ${line.name} `;
+          result += `${firstStop.name} (${firstStop.code}) - ${lastStop.name} (${lastStop.code})`;
+        }
 
-            line.arrivals.forEach(arrival => {
-              result += ` ${arrival.time}`;
-            })
+        result += '\n';
+      })
 
-            result += '\n';
-          });
-
-          resolve(result);
-        });
-      }
-    });
-  };
+      return resolve(result);
+    }
+  });
 }
